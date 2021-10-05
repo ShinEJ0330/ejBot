4d94a9d3e480c5340b05984435099795827c6c74
ShinEJ0330 <90272655+ShinEJ0330@users.noreply.github.com>
2021-10-06 00:57:30 +0900

    ej

--- discord bot/main.py
+++ discord bot/main.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import os
 
 def main():
-    prefix = ''
+    prefix = '-'
     intents = discord.Intents.all()
 
     client = commands.Bot(command_prefix=prefix, intents = intents)
@@ -19,22 +19,12 @@ def main():
 
     async def on_ready():
         await client.change_presence(status=discord.Status.online)
-        await client.change_presence(activity=discord.Game(name="실행"))
+        await client.change_presence(activity=discord.Activity(name="실행"))
         print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
 
-
-    @client.command(name = "새로고침")
-    async def _reload(ctx, extension):
-        client.unload_extension(f"cogs.{extension}")
-        client.load_extension(f'cogs.{extension}')
-        await ctx.send(f"{extension}이 새로고침 되었어요!")
-        
     for filename in os.listdir('./cogs'):
         if '.py' in filename:
             filename = filename.replace('.py','')
             client.load_extension(f"cogs.{filename}")
         
     client.run(os.environ['token'])
-
-if __name__ == '__main__':
-    main()
