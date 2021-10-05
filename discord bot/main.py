ccec92fd8243e31d79f5ef40153c6a8c005f29b3
ShinEJ0330 <90272655+ShinEJ0330@users.noreply.github.com>
2021-10-06 01:03:41 +0900

    Update main.py

--- discord bot/main.py
+++ discord bot/main.py
@@ -2,29 +2,20 @@ import discord
 from discord.ext import commands
 import os
 
-def main():
-    prefix = '-'
-    intents = discord.Intents.all()
+client = commands.Bot(command_prefix = '-')
 
-    client = commands.Bot(command_prefix=prefix, intents = intents)
+@client.event
+async def on_ready():
 
-    @client.event
-    async def on_member_join(member):
-        if member.dm_channel:
-            channel = member.dm_channel
-        else:
-            channel = await member.create_dm()
-        name = member.name
-        await channel.send(f"{name}님, 안녕하세요! \'설명'을 입력해주세요!")
+  # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
+  await client.change_presence(status=discord.Status.online)
 
-    async def on_ready():
-        await client.change_presence(status=discord.Status.online)
-        await client.change_presence(activity=discord.Activity(name="실행"))
-        print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
+  await client.change_presence(activity=discord.Activity(name="활성화"))
+  #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
+  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
+  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
+  
+  print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
 
-    for filename in os.listdir('./cogs'):
-        if '.py' in filename:
-            filename = filename.replace('.py','')
-            client.load_extension(f"cogs.{filename}")
-        
-    client.run(os.environ['token'])
+
+client.run(os.environ['token'])
